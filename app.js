053db0722d96762c6d01166373405b4fa729edc4
djalexkidd <alexpyr@outlook.fr>
2022-07-11 10:03:25 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require('cookie-parser')
+
+// Configuration de l'Active Directory
+const ActiveDirectory = require('activedirectory2');
+const ad_config = { url: process.env.AD_SERVER,
+               baseDN: process.env.AD_BASEDN,
+               username: process.env.AD_USERNAME,
+               password: process.env.AD_PASSWORD }
+const ad = new ActiveDirectory(ad_config);
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+
+// Page d'accueil
+app.get("/", async (req, res) => {
+    res.render('index.ejs');
+});
+
+// Hébergement du serveur sur le port 3000
+app.listen(3000, () => console.log("Server is running!"));
